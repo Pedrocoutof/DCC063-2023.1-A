e18169fa5960aad1fc4d65a8191e8851d6319e53
Pedro Couto <pedrocouto152001@gmail.com>
2023-04-07 15:35:52 -0300

    melhorando visualização

--- scripts/afd-live.js
+++ scripts/afd-live.js
@@ -65,14 +65,58 @@ function refreshGraph(){
     
     graph = getAFD();
 
+   // cross-link node objects
+   graph.links.forEach(link => {
+    const a = graph.nodes[link.source];
+    const b = graph.nodes[link.target];
+    if (!a || !b) {
+        return; // skip this link if either a or b is undefined
+    }
+    !a.neighbors && (a.neighbors = []);
+    !b.neighbors && (b.neighbors = []);
+    a.neighbors.push(b);
+    b.neighbors.push(a);
+
+    !a.links && (a.links = []);
+    !b.links && (b.links = []);
+    a.links.push(link);
+    b.links.push(link);
+});
+
+const highlightNodes = new Set();
+const highlightLinks = new Set();
+let hoverNode = null;
+
+
     const Graph = ForceGraph3D()
     (document.getElementById('3d-graph'))
         .height('500')
         .width('750')
         .graphData(graph)
-        .nodeLabel('id')
         .linkLabel('label')
-        .linkDirectionalArrowLength(2.5)
+        .linkThreeObjectExtend(true)
+        .nodeThreeObject(node => {
+            const sprite = new SpriteText(node.id);
+            sprite.material.depthWrite = false; // make sprite background transparent
+            sprite.color = node.color;
+            sprite.textHeight = 8;
+            return sprite;
+          })
+        .linkThreeObject(link => {
+          const sprite = new SpriteText(`${link.label}`);
+          sprite.color = 'lightgrey';
+          sprite.textHeight = 1.5;
+          return sprite;
+        })
+        .linkPositionUpdate((sprite, { start, end }) => {
+            const middlePos = Object.assign(...['x', 'y', 'z'].map(c => ({
+              [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
+            })));
+  
+            // Position sprite
+            Object.assign(sprite.position, middlePos);
+          })
+        .linkDirectionalArrowLength(2)
         .linkDirectionalArrowRelPos(1)
         .linkCurvature(0.3);
         
